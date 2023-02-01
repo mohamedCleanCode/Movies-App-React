38bf9e7809b29f182154a875a59390da6890fee3
mohamed <m635379@gmail.com>
2023-02-01 16:52:03 +0200

    add dispatch method to btns to handel it

--- src/components/AddPageCard.js
+++ src/components/AddPageCard.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./AddPageCard.css";
+import * as actions from "./context/actionsTypes";
+import { useMovieContext } from "./context/GlobalContext";
 
 const AddPageCard = (props) => {
+  const movieContext = useMovieContext();
   const { Title, Year, imdbID, Poster } = props.movie;
+  const statusWatchList = movieContext.watchList.find((movie) => {
+    return movie.imdbID === props.movie.imdbID;
+  });
+  const statusWatched = movieContext.watched.find((movie) => {
+    return movie.imdbID === props.movie.imdbID;
+  });
   return (
     <div className="add-page-card">
       {Poster ? (
@@ -14,8 +23,30 @@ const AddPageCard = (props) => {
         <p className="title">{Title}</p>
         <p className="year">{Year}</p>
         <div className="controls">
-          <button>Add To Watch List</button>
-          <button>Add To Watched</button>
+          <button
+            disabled={statusWatchList || statusWatched}
+            className="btn"
+            onClick={() =>
+              movieContext.dispatch({
+                type: actions.ADD_MOVIE_TO_WATCHLIST,
+                payload: props.movie,
+              })
+            }
+          >
+            Add To Watch List
+          </button>
+          <button
+            disabled={statusWatched}
+            className="btn"
+            onClick={() =>
+              movieContext.dispatch({
+                type: actions.ADD_MOVIE_TO_WATCHED,
+                payload: props.movie,
+              })
+            }
+          >
+            Add To Watched
+          </button>
         </div>
       </div>
     </div>
